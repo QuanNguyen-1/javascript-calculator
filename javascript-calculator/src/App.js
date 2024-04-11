5dd118d7874233201c96c699e651c5c5d23e1400
Quan Nguyen <nguyen.quan1818@yahoo.com>
2024-04-11 00:08:57 -0400

    answer only added to expression after math operation or equal pressed

--- javascript-calculator/src/App.js
+++ javascript-calculator/src/App.js
@@ -29,252 +29,243 @@ function App() {
         setAnswer((parseFloat(answer)/100).toString());
         break;
       /* (math operations)
-        if expression empty and answer is 0 or answer already includes operation, do nothing
+        if answer already includes operation or the expression is empty, do nothing
         if equal is recently pressed, set answer to operation and set the expression to the result and the operation
-        if answer is already an operation, set answer to new operation and replace last operation with new operation
-        if last char in answer is decimal, clear answer and last operation from expression and replace with new operation
-        else set answer to the math operation and add the operation to expression
+        if answer is already other operation, set answer to new operation and replace previous operation in expression with new one
+        else set answer to the math operation and add the current answer and operation to expression
       */
       case "divide":
-        if ((answer === "0" && !expression) || answer.includes("/")){
+        if (answer.includes("/")){
           break;
         } else if (equalPressed){
           setExpression(answer.concat("/"));
           setAnswer("/");
           setEqualPressed(false);
-        } else if (answer === "+" || answer === "-" || answer === "*" || answer === "/") {
+        } else if (answer === "+" || answer === "-" || answer === "*" ) {
           setAnswer("/");
-          setExpression((expression.slice(0,expression.length-1)).concat(answer));
-        } else if (answer.toString().charAt(answer.length-1) === ".") {
-          setExpression((expression.slice(0, expression.length - answer.length - 1)).concat("/"));
-          setAnswer("/");
+          setExpression((expression.slice(0,expression.length-1)).concat("/"));
         } else {
           setAnswer("/");
-          setExpression(expression.concat(answer));
+          setExpression(expression.concat(answer).concat("/"));
         }
         break;
       case "multiply":
-        if (answer === "0" && !expression){
+        if (answer.includes("*")){
           break;
         } else if (equalPressed){
           setExpression(answer.concat("*"));
           setAnswer("*");
           setEqualPressed(false);
-        } else if (answer === "+" || answer === "-" || answer === "*" || answer === "/") {
+        } else if (answer === "+" || answer === "-" || answer === "/" ) {
           setAnswer("*");
-          setExpression((expression.slice(0,expression.length-1)).concat(answer));
-        } else if (answer.toString().charAt(answer.length-1) === ".") {
-          setExpression((expression.slice(0, expression.length - answer.length - 1)).concat("*"));
-          setAnswer("*");
+          setExpression((expression.slice(0,expression.length-1)).concat("*"));
         } else {
           setAnswer("*");
-          setExpression(expression.concat(answer));
+          setExpression(expression.concat(answer).concat("*"));
         }
         break;
       case "plus":
-        if (answer === "0" && !expression){
+        if (answer === ("+")){
           break;
         } else if (equalPressed){
           setExpression(answer.concat("+"));
           setAnswer("+");
           setEqualPressed(false);
-        } else if (answer === "+" || answer === "-" || answer === "*" || answer === "/") {
+        } else if (answer === "/" || answer === "-" || answer === "*" ) {
           setAnswer("+");
-          setExpression((expression.slice(0,expression.length-1)).concat(answer));
-        } else if (answer.toString().charAt(answer.length-1) === ".") {
-          setExpression((expression.slice(0, expression.length - answer.length - 1)).concat("+"));
-          setAnswer("+");
+          setExpression((expression.slice(0,expression.length-1)).concat("+"));
         } else {
           setAnswer("+");
-          setExpression(expression.concat(answer));
+          setExpression(expression.concat(answer).concat("+"));
         }
         break;
       case "minus":
-        if (answer === "0" && !expression){
+        if (answer.includes("-")){
           break;
         } else if (equalPressed){
           setExpression(answer.concat("-"));
           setAnswer("-");
           setEqualPressed(false);
-        } else if (answer === "+" || answer === "-" || answer === "*" || answer === "/") {
+        } else if (answer === "+" || answer === "/" || answer === "*" ) {
           setAnswer("-");
-          setExpression((expression.slice(0,expression.length-1)).concat(answer));
-        } else if (answer.toString().charAt(answer.length-1) === ".") {
-          setExpression((expression.slice(0, expression.length - answer.length - 1)).concat("-"));
-          setAnswer("-");
+          setExpression((expression.slice(0,expression.length-1)).concat("-"));
         } else {
           setAnswer("-");
-          setExpression(expression.concat(answer));
+          setExpression(expression.concat(answer).concat("-"));
         }
         break;
       /* (decimal)
       if answer already includes a decimal, do nothing
+      if math operation or 0 is answer, set the answer to 0.
+      if equal recently pressed, set answer to 0. and empty expression
+      else (answer is a digit), add decimal to the answer
       */
       case "dot":
         if (answer.includes(".")){
           break;
         } else if (answer === "+" || answer === "-" || answer === "*" || answer === "/" || answer === "0") {
           setAnswer("0.");
-          setExpression(expression.concat(answer));
         } else if (equalPressed) {
           setAnswer("0.");
-          setExpression(answer);
+          setExpression("");
           setEqualPressed(false);
         } else {
           setAnswer(answer.concat("."));
-          setExpression(expression.concat(answer));
         }
         break;
-
+      /*(equal)
+      if answer is a math operation, evaluate the current expression and remove the last operation and set equal pressed to true
+      else (answer is digit), evaluate current expression with answer added to it and set equal pressed to true
+      */
       case "equal":
-
+        if (answer === "+" || answer === "-" || answer === "*" || answer === "/"){
+          setExpression((expression.slice(0,expression.length-1)).concat("="));
+          setAnswer(eval(expression.slice(0,expression.length-1)));
+          setEqualPressed(true);
+        } else {
+          setExpression((expression.concat(answer)).concat("="));
+          setAnswer(eval(expression.concat(answer)));
+          setEqualPressed(true);
+        }
+        break;
       /* (for numbers)
-      if answer only 0 and empty expression or 0 expression or if equal is pressed recently, answer and expression becomes num, 
-      if answer only 0 and last char in expression is 0, answer and last char in expression becomes num
-      else add num to answer and to expression
+      if answer is 0 set the answer to digit
+      if equal is recently pressed, set answer to digit and empty expression
+      else (if answer is digit not 0 and equal not recently pressed), add digit to answer
       */
       case "seven":
-        if((answer === "0" && (!expression || expression === "0")) || (equalPressed)){
+        if(answer === "0"){
           setAnswer("7");
-          setExpression("7");
+        } else if (equalPressed){
+          setAnswer("7");
+          setExpression("");
           setEqualPressed(false);
-        } else if(answer === "0" && expression.slice(-1) === "0"){
+        } else if(answer === "+" || answer === "-" || answer === "*" || answer === "/"){
           setAnswer("7");
-          setExpression((expression.slice(0,expression.length-1)).concat("7"));
-        } else {
+        }else {
           setAnswer(answer.concat("7"));
-          setExpression(expression.concat("7"));
         }
         break;
       case "eight":
-        if((answer === "0" && (!expression || expression === "0")) || (equalPressed)){
+        if(answer === "0" || (equalPressed)){
           setAnswer("8");
-          setExpression("8");
+        } else if (equalPressed){
+          setAnswer("8");
+          setExpression("");
           setEqualPressed(false);
-        } else if(answer === "0" && expression.slice(-1) === "0"){
+        } else if(answer === "+" || answer === "-" || answer === "*" || answer === "/"){
           setAnswer("8");
-          setExpression((expression.slice(0,expression.length-1)).concat("8"));
         } else {
           setAnswer(answer.concat("8"));
-          setExpression(expression.concat("8"));
         }
         break;
       case "nine":
-        if((answer === "0" && (!expression || expression === "0")) || (equalPressed)){
+        if(answer === "0" || (equalPressed)){
           setAnswer("9");
-          setExpression("9");
+        } else if (equalPressed){
+          setAnswer("9");
+          setExpression("");
           setEqualPressed(false);
-        } else if(answer === "0" && expression.slice(-1) === "0"){
+        } else if(answer === "+" || answer === "-" || answer === "*" || answer === "/"){
           setAnswer("9");
-          setExpression((expression.slice(0,expression.length-1)).concat("9"));
         } else {
           setAnswer(answer.concat("9"));
-          setExpression(expression.concat("9"));
         }
         break;
       case "four":
-        if((answer === "0" && (!expression || expression === "0")) || (equalPressed)){
+        if(answer === "0" || (equalPressed)){
           setAnswer("4");
-          setExpression("4");
+        } else if (equalPressed){
+          setAnswer("4");
+          setExpression("");
           setEqualPressed(false);
-        } else if(answer === "0" && expression.slice(-1) === "0"){
+        } else if(answer === "+" || answer === "-" || answer === "*" || answer === "/"){
           setAnswer("4");
-          setExpression((expression.slice(0,expression.length-1)).concat("4"));
         } else {
           setAnswer(answer.concat("4"));
-          setExpression(expression.concat("4"));
         }
         break;
       case "five":
-        if((answer === "0" && (!expression || expression === "0")) || (equalPressed)){
+        if(answer === "0" || (equalPressed)){
           setAnswer("5");
-          setExpression("5");
+        } else if (equalPressed){
+          setAnswer("5");
+          setExpression("");
           setEqualPressed(false);
-        } else if(answer === "0" && expression.slice(-1) === "0"){
+        } else if(answer === "+" || answer === "-" || answer === "*" || answer === "/"){
           setAnswer("5");
-          setExpression((expression.slice(0,expression.length-1)).concat("5"));
         } else {
           setAnswer(answer.concat("5"));
-          setExpression(expression.concat("5"));
         }
         break;
       case "six":
-        if((answer === "0" && (!expression || expression === "0")) || (equalPressed)){
+        if(answer === "0" || (equalPressed)){
           setAnswer("6");
-          setExpression("6");
+        } else if (equalPressed){
+          setAnswer("6");
+          setExpression("");
           setEqualPressed(false);
-        } else if(answer === "0" && expression.slice(-1) === "0"){
+        } else if(answer === "+" || answer === "-" || answer === "*" || answer === "/"){
           setAnswer("6");
-          setExpression((expression.slice(0,expression.length-1)).concat("6"));
         } else {
           setAnswer(answer.concat("6"));
-          setExpression(expression.concat("6"));
         }
         break;
       case "one":
-        if((answer === "0" && (!expression || expression === "0")) || (equalPressed)){
+        if(answer === "0" || (equalPressed)){
           setAnswer("1");
-          setExpression("1");
+        } else if (equalPressed){
+          setAnswer("1");
+          setExpression("");
           setEqualPressed(false);
-        } else if(answer === "0" && expression.slice(-1) === "0"){
+        } else if(answer === "+" || answer === "-" || answer === "*" || answer === "/"){
           setAnswer("1");
-          setExpression((expression.slice(0,expression.length-1)).concat("1"));
         } else {
           setAnswer(answer.concat("1"));
-          setExpression(expression.concat("1"));
         }
         break;
       case "two":
-        if((answer === "0" && (!expression || expression === "0")) || (equalPressed)){
+        if(answer === "0" || (equalPressed)){
           setAnswer("2");
-          setExpression("2");
+        } else if (equalPressed){
+          setAnswer("2");
+          setExpression("");
           setEqualPressed(false);
-
-        } else if(answer === "0" && expression.slice(-1) === "0"){
+        } else if(answer === "+" || answer === "-" || answer === "*" || answer === "/"){
           setAnswer("2");
-          setExpression((expression.slice(0,expression.length-1)).concat("2"));
         } else {
           setAnswer(answer.concat("2"));
-          setExpression(expression.concat("2"));
         }
         break;
       case "three":
-        if((answer === "0" && (!expression || expression === "0")) || (equalPressed)){
+        if(answer === "0" || (equalPressed)){
           setAnswer("3");
-          setExpression("3");
+        } else if (equalPressed){
+          setAnswer("3");
+          setExpression("");
           setEqualPressed(false);
-        } else if(answer === "0" && expression.slice(-1) === "0"){
+        } else if(answer === "+" || answer === "-" || answer === "*" || answer === "/"){
           setAnswer("3");
-          setExpression((expression.slice(0,expression.length-1)).concat("3"));
         } else {
           setAnswer(answer.concat("3"));
-          setExpression(expression.concat("3"));
         }
         break;
-      /* (for zero)
-      if answer is 0 and there is empty expression, set expression to 0 
-      if answer is 0 and there is expression already, set answer to 0
-      if equal recently pressed, set answer to 0 and empty expression
-      if answer is math operation, set answer to 0 and add 0 to expression
-      else add zero to answer and expression
-      */
       case "zero":
-        if(answer === "0" && !expression){
-          setExpression("0");
-        } else if(answer === "0" && expression) {
+        if(answer === "0" || (equalPressed)){
           setAnswer("0");
-        } else if(equalPressed) {
+        } else if (equalPressed){
           setAnswer("0");
           setExpression("");
           setEqualPressed(false);
-        } else if(answer === "+" || answer === "-" || answer === "*" || answer === "/") {
+        } else if(answer === "+" || answer === "-" || answer === "*" || answer === "/"){
           setAnswer("0");
-          setExpression(expression.concat("0"));
         } else {
           setAnswer(answer.concat("0"));
-          setExpression(expression.concat("0"));
         }
         break;
+      default:
+        setAnswer(answer);
+
     }
   }
 
